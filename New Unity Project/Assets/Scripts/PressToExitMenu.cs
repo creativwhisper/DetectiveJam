4a42e0b8b306cbdaae42f160c8e498b4f870a48c
Unknown <creativewhisperings@gmail.com>
2018-03-17 20:31:39 +0100

    Some changes on secondary scenes

---
+++ New Unity Project/Assets/Scripts/PressToExitMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressToExitMenu : MonoBehaviour {
+
+	
+	void Update () {
+        if (Input.GetKeyDown("q"))
+            SceneManager.LoadScene("Menu");
+	}
+}
